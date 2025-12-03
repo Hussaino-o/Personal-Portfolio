7a6a4d0d69870884bbb53efd621fa2ba955da5d1
Hussain Ali <112756072+Hussaino-o@users.noreply.github.com>
2025-12-03 16:22:55 +0300

    Complete the Portfolio

--- main.js
+++ main.js
@@ -18,32 +18,32 @@ const testimonialsCardOverlay = document.querySelector(
 
 let testimonialsInfo = {
   DanielLewis: {
-    imgurl: "https://i.postimg.cc/zGDHfn3G/avatar-1.png",
-    name: "Daniel Lewis",
+    imgurl: "images/testimonials-test-avatar.png",
+    name: "Ahmed Juad",
     time: "14 June, 2024",
     testimonial:
-      "Richard was hired to create a corporate identity. It's modern, clean and with a beautiful design that got a lot of praises from colleagues and visitors. We were very pleased with the work done. He has a lot of experience and is very concerned about the needs of client",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt libero at nostrum voluptate dignissimos voluptatum veniam aperiam quisquam nobis aliquid id sunt fugit, facilis illum corrupti et cum dolore nam?",
   },
   JessicaMiller: {
-    imgurl: "https://i.postimg.cc/DwY0yHtx/avatar-2.png",
-    name: "Jessica Miller",
+    imgurl: "images/testimonials-test-avatar.png",
+    name: "Mostafa Kamel",
     time: "17 September, 2021",
     testimonial:
-      "Working with Richard has been an absolute pleasure. I was impressed with his attention to detail, his web design skills and his professional approach to our timelines and processes.",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt libero at nostrum voluptate dignissimos voluptatum veniam aperiam quisquam nobis aliquid id sunt fugit, facilis illum corrupti et cum dolore nam?",
   },
   EmilyEvans: {
-    imgurl: "https://i.postimg.cc/fRFWhX9F/avatar-3.png",
-    name: "Emily Evans",
+    imgurl: "images/testimonials-test-avatar.png",
+    name: "Momel abed",
     time: "5 July, 2025",
     testimonial:
-      "I couldn't be happier with the website that Richard created for us. His attention to detail and creativity is unmatched. Our clients frequently compliment the design, and it has significantly improved our brand image.",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt libero at nostrum voluptate dignissimos voluptatum veniam aperiam quisquam nobis aliquid id sunt fugit, facilis illum corrupti et cum dolore nam?",
   },
   HenryWilliams: {
-    imgurl: "https://i.postimg.cc/zXv1Xv81/avatar-4.png",
-    name: "henry Williams",
+    imgurl: "images/testimonials-test-avatar.png",
+    name: "Ali Nasser",
     time: "25 November, 2022",
     testimonial:
-      "I was overwhelmed with the thought of redesigning my online store, but Richard made the process seamless. The site is not only visually appealing but also optimized for conversions. I've seen a 50% increase in traffic since the launch!",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt libero at nostrum voluptate dignissimos voluptatum veniam aperiam quisquam nobis aliquid id sunt fugit, facilis illum corrupti et cum dolore nam?",
   },
 };
 
